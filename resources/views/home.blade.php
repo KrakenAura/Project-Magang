d554aa53c6cc2bffb2ea9f744b3a0fccaac3c012
KrakenAura <akarim18641@gmail.com>
2024-07-19 10:12:31 +0700

    rearrage folder structure

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
     <link rel="stylesheet" href="{{asset('css/home.css')}}">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@48,400,0,0" />
     <title>Document</title>
