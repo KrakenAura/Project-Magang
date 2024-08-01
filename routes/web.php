21b069affaea5e690edc971c317c0ae66533c273
Muhammad Rafid <96788115+Rafid-28@users.noreply.github.com>
2024-08-01 10:26:37 +0700

    dashbord admin program tv

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::get('/layanan-publik', function () {
     return view('layananpublik');
 });
 Route::get('/kerja', function () {
-    return view('dashboard/admin_layananpublik_lihakomen');
+    return view('dashboard/admin_linktv');
 });
 
 Route::get('/kabar-balai-kota', function () {
