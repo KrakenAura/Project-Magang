8fdf50a133f6a7bbd9714ab20ce2d3ab8336594e
Muhammad Rafid <96788115+Rafid-28@users.noreply.github.com>
2024-08-01 14:35:18 +0700

    admin_linktv

--- resources/views/dashboard/admin_linktv.blade.php
+++ resources/views/dashboard/admin_linktv.blade.php
@@ -1,42 +1,47 @@
 @extends('dashboard/adminlayouts')
 
 @section('css')
+<link rel="stylesheet" href="{{asset('/css/admin_library.css')}}">
 
 @endsection
 
 @section('content')
 
-<div class="link-live">
+<div class="library">
     <table class="table">
         <thead>
             <tr>
-                <th>Nama</th>
-                <th>Link</th>
-                <th>Action</th>
+                <th style="width: 30%">Gambar</th>
+                <th style="width: 30%">Nama</th>
+                <th style="width: 30%">Link</th>
+                <th style="width: 10%">Action</th>
             </tr>
         </thead>
         <tbody>
+            @foreach ($librarys as $library)
             <tr>
-                <td>TES</td>
-                <td><a href="#">https://streaming.com</a></td>
+                <td><img src="{{ asset('storage/'.$library->image) }}" alt="{{ $library->title }}" class="library-image"></td>
+                <td>{{ $library->judul }}</td>
+                <td><a href="{{ $library->link }}" target="_blank">{{ $library->link }}</a></td>
                 <td>
                     <button type="button" class="btn btn-primary btn-sm" data-toggle="modal" data-target="#exampleModal">Edit</button>
                 </td>
             </tr>
+            @endforeach
         </tbody>
+
         <!-- Modal -->
         <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div class="modal-dialog" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">Edit</h5>
+                        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                         </button>
                     </div>
                     <div class="modal-body">
-                        <label for="exampleInputEmail1">Ganti Link</label>
-                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter new Link">
+                        ...
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
@@ -46,6 +51,7 @@
             </div>
         </div>
     </table>
+    <a href="{{route('admin.library_tambah')}}"><button type="button" class="btn btn-success">+ Tambah Library</button></a>
 </div>
 
 @endsection
