4650e0de1f856f840a28bf8f3d73083056c8ba9d
Muhammad Rafid <96788115+Rafid-28@users.noreply.github.com>
2024-08-02 09:00:59 +0700

    admin galeri

--- resources/views/dashboard/admin_galeri.blade.php
+++ resources/views/dashboard/admin_galeri.blade.php
@@ -8,10 +8,10 @@
 <div class="content">
     <div class="gallery-container">
         @foreach ( $galeries as $galeri)
-
         <div class="card galery-card">
             <div class="card-image">
-                <img src="{{asset('storage/Galery_images/'.$galeri->image)}}" alt={{$galeri->title}}>
+                <!-- <img src="{{asset('storage/Galery_images/'.$galeri->image)}}" alt={{$galeri->title}}> -->
+                <img src="{{asset('images/banner4.jpg')}}">
             </div>
             <div class="card-desc">
                 <h4>{{$galeri->title}}</h4>
@@ -22,6 +22,10 @@
             </div>
         </div>
     </div>
+
+    <button type="button" class="btn btn-success" data-toggle="modal" data-target="#modalTambah">+ Tambah Library</button>
+
+
 
     <!--Modal -->
     <div class="modal fade" id="modalUpdate{{$galeri->id}}" tabindex="-1" role="dialog" aria-labelledby="modalUpdate" aria-hidden="true">
@@ -38,10 +42,6 @@
                     </div>
                     <div class="modal-body">
                         <div class="form-group">
-                            @php
-                            $debug_id = $galeri->id;
-                            @endphp
-                            <p>Debug ID: {{ $debug_id }}</p>
                             <label for="UpdateDeskripsi">Change Description</label>
                             <input type="text" class="form-control" id="UpdateDeskripsi" name="title" placeholder="Enter New Description" value="{{$galeri->title}}">
 
@@ -85,6 +85,39 @@
                         </div>
                         <div class="mb-3">
                             <label for="formFile" class="form-label">Change Image</label>
+                            <input type="file" name="image" id="banner-image" class="banner-input" accept="image/*">
+                        </div>
+
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary">Save changes</button>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>
+
+    <div class="modal fade" id="modalTambah" tabindex="-1" role="dialog" aria-labelledby="modalUpdate" aria-hidden="true">
+        <form action="{{ route('UpdateGaleri', $galeri->id) }}" method="POST" enctype="multipart/form-data">
+            @csrf
+            @method('PUT')
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="UpdateImage">Choose Images</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="form-group">
+                            <label for="UpdateDeskripsi">Description</label>
+                            <input type="text" class="form-control" id="UpdateDeskripsi" name="title" placeholder="Enter Description">
+
+                        </div>
+                        <div class="mb-3">
+                            <label for="formFile" class="form-label">Choose Image</label>
                             <input type="file" name="image" id="banner-image" class="banner-input" accept="image/*">
                         </div>
 
--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,7 @@ Route::get('/layanan-publik', function () {
     return view('layananpublik');
 });
 Route::get('/kerja', function () {
-    return view('dashboard/admin_linktv');
+    return view('dashboard/admin_layananpublik_lihakomen');
 });
 
 Route::get('/kabar-balai-kota', function () {
